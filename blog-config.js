948b6ff42f7dcafade68a46bd33cee0f4b904d70
HyunJoo Kim <hyunjoogo@gmail.com>
2023-03-16 18:05:12 +0900

    Update blog-config.js

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "hoodie",
   description: "Hello :) I'm Hudi who developed gatsby-starter-hoodie theme.",
   author: "Hudi",
-  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  siteUrl: "https://hyunjoogo.github.io/lanky-blog/",
   links: {
     github: "https://github.com/devHudi",
     linkedIn: "https://linkedin.com",
@@ -11,7 +11,7 @@ module.exports = {
     email: "mailto:devhudi@gmail.com",
   },
   utterances: {
-    repo: "devHudi/gatsby-starter-hoodie",
+    repo: "hyunjoogo/lanky-blog",
     type: "pathname",
   },
 }
